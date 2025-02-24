acc8d01374f7c59a1e8ed72de81d90ba8c764aaf
Lloyd Leano <leanolloyd@gmail.com>
2025-02-24 18:00:50 +0800

    [views.py] Editted def recipe_with_param(): completed the code via given recipe lists respectively

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -7,5 +7,68 @@ def recipe_list(request):
     template = loader.get_template('recipe_list.html')
     return HttpResponse(template.render())
 
-#def recipe_with_param(request, param="list"):
-    #template for recipe 1 & 2
+def recipe_with_param(request, param="list"):
+    recipe = "empty string"
+    if param == '1':
+        recipe = {
+    "name": "Recipe 1",
+    "ingredients": [
+        {
+            "name": "tomato",
+            "quantity": "3pcs"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "pork",
+            "quantity": "1kg"
+        },
+        {
+            "name": "water",
+            "quantity": "1L"
+        },
+        {
+            "name": "sinigang mix",
+            "quantity": "1 packet"
+        }
+    ],
+    "link": "/recipe/1"
+}
+    elif param == '2':
+        recipe = {
+    "name": "Recipe 2",
+    "ingredients": [
+        {
+            "name": "garlic",
+            "quantity": "1 head"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "vinegar",
+            "quantity": "1/2cup"
+        },
+        {
+            "name": "water",
+            "quantity": "1 cup"
+        },
+        {
+            "name": "salt",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "whole black peppers",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "pork",
+            "quantity": "1 kilo"
+        }
+    ],
+    "link": "/recipe/2"
+}
+    return render(request, 'recipe_id.html', {'recipe':recipe})
