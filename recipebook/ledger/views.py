d433cb2d5d392b037e2c0ee689c5a1a3fa51005b
Lloyd Leano <leanolloyd@gmail.com>
2025-02-24 16:47:55 +0800

    Added urls.py in ledger and edited the views.py with temporary defand edited urls.py accordingly

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def recipe_list(request):
+    #template for recipelist
+
+def recipe_with_param(request, param="list"):
+    #template for recipe 1 & 2
