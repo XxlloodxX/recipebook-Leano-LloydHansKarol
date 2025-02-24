8b4a0b9bc3d2beb4fe6ce8962d398252411c43be
Lloyd Leano <leanolloyd@gmail.com>
2025-02-24 17:54:44 +0800

    [views.py] Editted def recipe_list(): changed it to load recipe_list.html

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def recipe_list(request):
-    return HttpResponse("template")
+    template = loader.get_template('recipe_list.html')
+    return HttpResponse(template.render())
 
 #def recipe_with_param(request, param="list"):
     #template for recipe 1 & 2
