b8cfce41d942adb76f5fdee7c706f2d12525c6dd
Lloyd Leano <leanolloyd@gmail.com>
2025-03-07 15:33:21 +0800

    [views.py][urls.py] Removed recipe_populate() and urls

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from .views import recipe_list, recipe_with_param, recipe_populate
+from .views import recipe_list, recipe_with_param
 
 urlpatterns = [
     path('recipes/list', recipe_list, name="recipe_list"),
     path('recipe/<int:param>', recipe_with_param, name="recipe_with_param"),
-    path('recipes/populate', recipe_populate, name="recipe_populate"),
     ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -13,20 +13,3 @@ def recipe_with_param(request, param):
     ingredients = RecipeIngredient.objects.filter(recipe=recipe)
         
     return render(request, 'recipe_id.html', {'recipe':recipe, 'ingredients':ingredients})
-
-def recipe_populate(request):
-    RecipeIngredient.objects.create(quantity = "3pcs", ingredients = Ingredient.objects.get(name="tomato"), recipe = Recipe.objects.get(name="Recipe 1"))
-    RecipeIngredient.objects.create(quantity = "1pc", ingredients = Ingredient.objects.get(name="onion"), recipe = Recipe.objects.get(name="Recipe 1"))
-    RecipeIngredient.objects.create(quantity = "1kg", ingredients = Ingredient.objects.get(name="pork"), recipe = Recipe.objects.get(name="Recipe 1"))
-    RecipeIngredient.objects.create(quantity = "1L", ingredients = Ingredient.objects.get(name="water"), recipe = Recipe.objects.get(name="Recipe 1"))
-    RecipeIngredient.objects.create(quantity = "1 packet", ingredients = Ingredient.objects.get(name="sinigang mix"), recipe = Recipe.objects.get(name="Recipe 1"))
-    
-    RecipeIngredient.objects.create(quantity = "1 head", ingredients = Ingredient.objects.get(name="garlic"), recipe = Recipe.objects.get(name="Recipe 2"))
-    RecipeIngredient.objects.create(quantity = "1 pc", ingredients = Ingredient.objects.get(name="onion"), recipe = Recipe.objects.get(name="Recipe 2"))
-    RecipeIngredient.objects.create(quantity = "1/2cup", ingredients = Ingredient.objects.get(name="vinegar"), recipe = Recipe.objects.get(name="Recipe 2"))
-    RecipeIngredient.objects.create(quantity = "1 cup", ingredients = Ingredient.objects.get(name="water"), recipe = Recipe.objects.get(name="Recipe 2"))
-    RecipeIngredient.objects.create(quantity = "1 tablespoon", ingredients = Ingredient.objects.get(name="salt"), recipe = Recipe.objects.get(name="Recipe 2"))
-    RecipeIngredient.objects.create(quantity = "1 tablespoon", ingredients = Ingredient.objects.get(name="whole black peppers"), recipe = Recipe.objects.get(name="Recipe 2"))
-    RecipeIngredient.objects.create(quantity = "1 kilo", ingredients = Ingredient.objects.get(name="pork"), recipe = Recipe.objects.get(name="Recipe 2"))
-
-    return HttpResponse("Added Recipes and Ingredients")
