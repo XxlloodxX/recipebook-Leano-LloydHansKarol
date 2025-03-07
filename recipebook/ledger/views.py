d51a9ee1093607c60af892fc356589865d0b401d
Lloyd Leano <leanolloyd@gmail.com>
2025-03-07 15:19:54 +0800

    [views.py] Imported the three models and changed the recipe_list() & recipe_with_param() accordingly.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,74 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Ingredient, Recipe, RecipeIngredient
 
 # Create your views here.
 def recipe_list(request):
-    template = loader.get_template('recipe_list.html')
-    return HttpResponse(template.render())
+    recipes = Recipe.objects.all()
+    return render(request, 'recipe_list.html', {'recipe': recipes})
 
-def recipe_with_param(request, param="list"):
-    recipe = "empty string"
-    if param == '1':
-        recipe = {
-    "name": "Recipe 1",
-    "ingredients": [
-        {
-            "name": "tomato",
-            "quantity": "3pcs"
-        },
-        {
-            "name": "onion",
-            "quantity": "1pc"
-        },
-        {
-            "name": "pork",
-            "quantity": "1kg"
-        },
-        {
-            "name": "water",
-            "quantity": "1L"
-        },
-        {
-            "name": "sinigang mix",
-            "quantity": "1 packet"
-        }
-    ],
-    "link": "/recipe/1"
-}
-    elif param == '2':
-        recipe = {
-    "name": "Recipe 2",
-    "ingredients": [
-        {
-            "name": "garlic",
-            "quantity": "1 head"
-        },
-        {
-            "name": "onion",
-            "quantity": "1pc"
-        },
-        {
-            "name": "vinegar",
-            "quantity": "1/2cup"
-        },
-        {
-            "name": "water",
-            "quantity": "1 cup"
-        },
-        {
-            "name": "salt",
-            "quantity": "1 tablespoon"
-        },
-        {
-            "name": "whole black peppers",
-            "quantity": "1 tablespoon"
-        },
-        {
-            "name": "pork",
-            "quantity": "1 kilo"
-        }
-    ],
-    "link": "/recipe/2"
-}
-    return render(request, 'recipe_id.html', {'recipe':recipe})
+def recipe_with_param(request, param):
+    recipe = Recipe.objects.get(id = param)
+    ingredients = RecipeIngredient.objects.filter(recipe=recipe)
+        
+    return render(request, 'recipe_id.html', {'recipe':recipe, 'ingredients':ingredients})
