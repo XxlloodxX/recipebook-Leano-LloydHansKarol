7efd260152c52e6fbf47b76e8e565d6cb5af01f9
Lloyd Leano <leanolloyd@gmail.com>
2025-03-07 15:26:25 +0800

    [urls.py] Imported recipe_populate in .views and added recipes/populate to add the data.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import recipe_list, recipe_with_param
+from .views import recipe_list, recipe_with_param, recipe_populate
 
 urlpatterns = [
     path('recipes/list', recipe_list, name="recipe_list"),
-    path('recipe/<param>', recipe_with_param, name="recipe_with_param"),
+    path('recipe/<int:param>', recipe_with_param, name="recipe_with_param"),
+    path('recipes/populate', recipe_populate, name="recipe_populate"),
     ]
 
 app_name = "ledger"
