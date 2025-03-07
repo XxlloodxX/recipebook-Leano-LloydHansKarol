500755f0b96f788cf7a880e7d69ed24b09819c84
Lloyd Leano <leanolloyd@gmail.com>
2025-03-07 15:23:20 +0800

    [views.py] Added def recipe_populate() to add in variables to the data base

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -13,3 +13,20 @@ def recipe_with_param(request, param):
     ingredients = RecipeIngredient.objects.filter(recipe=recipe)
         
     return render(request, 'recipe_id.html', {'recipe':recipe, 'ingredients':ingredients})
+
+def recipe_populate(request):
+    Recipe.objects.create(name = "Recipe 1")
+    Recipe.objects.create(name = "Recipe 2")
+
+    Ingredient.objects.create(name = "tomato")
+    Ingredient.objects.create(name = "onion")
+    Ingredient.objects.create(name = "pork")
+    Ingredient.objects.create(name = "water")
+    Ingredient.objects.create(name = "sinigang mix")
+    
+    Ingredient.objects.create(name = "garlic")
+    Ingredient.objects.create(name = "vinegar")
+    Ingredient.objects.create(name = "salt")
+    Ingredient.objects.create(name = "whole black peppers")
+
+    return HttpResponse("Added Recipes and Ingredients")
