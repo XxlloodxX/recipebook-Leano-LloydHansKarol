59f5d9bd3bb1f779c544b595a284890e6fafe50d
Lloyd Leano <leanolloyd@gmail.com>
2025-03-20 18:42:19 +0800

    [models.py] Added Profile model and edited Recipe model.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
