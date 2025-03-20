dcbc5e8f695877e7fd18fda44280746fe1253ddb
Lloyd Leano <leanolloyd@gmail.com>
2025-03-20 19:01:28 +0800

    [urls.py] Imported a redirect path and added a path for '' to redirect automatically to recipe_list.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
 from .views import recipe_list, recipe_with_param
 
 urlpatterns = [
+    path('', lambda request: redirect('ledger:recipe_list'), name="home"),
     path('recipes/list', recipe_list, name="recipe_list"),
     path('recipe/<int:param>', recipe_with_param, name="recipe_with_param"),
     ]
