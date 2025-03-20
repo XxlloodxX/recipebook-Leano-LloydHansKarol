5f78264d7ee7db6181fb2ce6f681c93d5b3cbffc
Lloyd Leano <leanolloyd@gmail.com>
2025-03-20 18:59:18 +0800

    [views.py] Imported login_required and assigned @login_required for the def functions.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 from .models import Ingredient, Recipe, RecipeIngredient
 
 # Create your views here.
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipe_list.html', {'recipe': recipes})
 
+@login_required
 def recipe_with_param(request, param):
     recipe = Recipe.objects.get(id = param)
     ingredients = RecipeIngredient.objects.filter(recipe=recipe)
