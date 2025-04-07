3a540c0d992fe7f84e6d82fa42b091969487a9d7
Lloyd Leano <leanolloyd@gmail.com>
2025-04-07 17:08:06 +0800

    [views.py] Imported forms, models, redirects, get_object_or_404. Implemented def recipe_add function to add Recipes, Ingredients, and RecipeIngredients.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Ingredient, Recipe, RecipeIngredient, RecipeImage
+from .forms import RecipeForm, RecipeIngredientForm, IngredientForm, RecipeImageForm
 
-# Create your views here.
+
 @login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
@@ -16,3 +17,30 @@ def recipe_with_param(request, param):
     ingredients = RecipeIngredient.objects.filter(recipe=recipe)
         
     return render(request, 'recipe_id.html', {'recipe':recipe, 'ingredients':ingredients})
+
+@login_required
+def recipe_add(request):
+    recipe_form = RecipeForm(request.POST or None)
+    ingredient_form = IngredientForm(request.POST or None)
+    recipe_ingredient_form = RecipeIngredientForm(request.POST or None)
+
+    if (request.method == 'POST'):
+        if 'create_recipe' in request.POST and recipe_form.is_valid():
+            recipe = recipe_form.save(commit=False)
+            recipe.author = request.user.profile  
+            recipe.save()
+            return redirect('ledger:recipe_add')
+
+        elif 'create_ingredient' in request.POST and ingredient_form.is_valid():
+            ingredient_form.save() 
+            return redirect('ledger:recipe_add')  
+
+        elif 'create_recipe_ingredient' in request.POST and recipe_ingredient_form.is_valid():
+            recipe_ingredient_form.save()
+            return redirect('ledger:recipe_add')
+
+    return render(request, 'recipe_create.html', {
+        'recipe_form': recipe_form,
+        'ingredient_form': ingredient_form,
+        'recipe_ingredient_form': recipe_ingredient_form,
+    })
