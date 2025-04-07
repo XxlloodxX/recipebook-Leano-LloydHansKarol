267a56ca45ab2ca2c0e2c722c277798cd9bb424a
Lloyd Leano <leanolloyd@gmail.com>
2025-04-07 16:52:53 +0800

    [models.py] Created and implemented class RecipeImage model.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
@@ -15,6 +15,11 @@ class Recipe(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_on = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to='recipe_images/', null=False)
+    description = models.CharField(max_length=255, blank=True)
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE, related_name='images')
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
