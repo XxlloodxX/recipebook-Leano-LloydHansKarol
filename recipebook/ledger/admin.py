57782fb6f25ff164af44f138e43817798ed289d8
Lloyd Leano <leanolloyd@gmail.com>
2025-04-07 16:59:30 +0800

    [admin.py] Created admin panel: RecipeImageAdmin, and created RecipeImageInline. Imported RecipeImage.

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin 
 from django.contrib.auth.models import User
-from .models import Recipe, Ingredient, RecipeIngredient, Profile
+from .models import Recipe, Ingredient, RecipeIngredient, Profile, RecipeImage
 
-# Register your models here.
+
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+    extra = 1
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     list_display = ('name',)
+    inlines = [RecipeImageInline]
+
+class RecipeImageAdmin(admin.ModelAdmin):
+    list_display = ('image', 'description', 'recipe')
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
@@ -29,3 +37,4 @@ admin.site.register(User, UserAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(RecipeImage, RecipeImageAdmin)
