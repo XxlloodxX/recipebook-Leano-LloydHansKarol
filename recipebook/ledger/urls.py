16203fbf5f48fab942392994c3a3f6053c10d4a0
Lloyd Leano <leanolloyd@gmail.com>
2025-04-07 17:10:25 +0800

    [ledger/urls.py] Imported recipe_add and created a path in accordance.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 from django.shortcuts import redirect
-from .views import recipe_list, recipe_with_param
+from .views import recipe_list, recipe_with_param, recipe_add
 
 urlpatterns = [
     path('', lambda request: redirect('ledger:recipe_list'), name="home"),
     path('recipes/list', recipe_list, name="recipe_list"),
     path('recipe/<int:param>', recipe_with_param, name="recipe_with_param"),
+    path('recipe/add/', recipe_add, name="recipe_add"),
     ]
 
 app_name = "ledger"
