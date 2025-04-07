64d9fb29cd1b4ca4740aea6d146937256c24a088
Lloyd Leano <leanolloyd@gmail.com>
2025-04-07 16:56:44 +0800

    [ledger] Created forms.py which contains: RecipeForm, RecipeIngredientForm, IngredientForm, RecipeImageForm.

---
+++ recipebook/ledger/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Recipe, Ingredient, RecipeIngredient, RecipeImage
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['name']
+
+class RecipeIngredientForm(forms.ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = ['quantity', 'ingredients', 'recipe']
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name']
+
+class RecipeImageForm(forms.ModelForm):
+    class Meta:
+        model = RecipeImage
+        fields = ['image', 'description']
